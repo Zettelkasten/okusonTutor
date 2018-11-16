4075bd77293cc32706bc1ab90d7ce16375d47dea
Lars Göttgens <lars.goettgens@rwth-aachen.de>
2018-11-16 15:44:22 +0100

    add more html parsing

--- okusonTutor.js
+++ okusonTutor.js
@@ -4,3 +4,17 @@ function getGroupNr() {
     groupNr = parseInt(/(Gruppe|Group number):\W*(\d{1,2})/gm.exec(text)[2]);
     return groupNr > 0 ? groupNr : null;
 }
+
+function getExerciseNr() {
+    var exNr;
+    text = document.getElementsByTagName('html')[0].innerText;
+    exNr = parseInt(/Blatt:\W*(\d{1,2})/gm.exec(text)[1]);
+    return exNr;
+}
+
+function getLecture() {
+    var lecture = "";
+    text = document.getElementsByTagName('html')[0].innerText;
+    lecture = /Vorlesung:\W*(.*)\n/gm.exec(text)[1];
+    return lecture;
+}
