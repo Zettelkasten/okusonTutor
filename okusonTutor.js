b55832dd9879ea6429b749a0703248e6e4d732db
Lars Göttgens <lars.goettgens@rwth-aachen.de>
2018-11-22 21:49:54 +0100

    add names and refactor

--- okusonTutor.js
+++ okusonTutor.js
@@ -2,10 +2,12 @@ class Student {
     static from(obj) {
         obj.setPoints = Student.prototype.setPoints;
         return obj;
-      }
+    }
 
-    constructor(matrNr) {
+    constructor(matrNr, firstName, lastName) {
         this.matrNr = matrNr;
+        this.firstName = firstName;
+        this.lastName = lastName;
         this.points = new Object();
     }
 
@@ -16,42 +18,50 @@ class Student {
 }
 
 function getGroupNr() {
-    var groupNr;
-    text = document.getElementsByTagName('html')[0].innerText;
-    groupNr = parseInt(/(Gruppe|Group number):\W*(\d{1,2})/gm.exec(text)[2]);
+    var text = document.getElementsByTagName('html')[0].innerText;
+    var groupNr = parseInt(/(Gruppe|Group number):\W*(\d{1,2})/gm.exec(text)[2]);
     return groupNr > 0 ? groupNr : null;
 }
 
 function getExerciseNr() {
-    var exNr;
-    text = document.getElementsByTagName('html')[0].innerText;
-    exNr = parseInt(/Blatt:\W*(\d{1,2})/gm.exec(text)[1]);
+    var text = document.getElementsByTagName('html')[0].innerText;
+    var exNr = parseInt(/Blatt:\W*(\d{1,2})/gm.exec(text)[1]);
     return exNr;
 }
 
 function getLecture() {
-    var lecture = "";
-    text = document.getElementsByTagName('html')[0].innerText;
-    lecture = /Vorlesung:\W*(.*)\n/gm.exec(text)[1];
+    var text = document.getElementsByTagName('html')[0].innerText;
+    var lecture = /Vorlesung:\W*(.*)\n/gm.exec(text)[1];
     return lecture;
 }
 
 function extractData() {
+    var data = new Map();
+    var html = document.getElementsByTagName('html')[0].innerHTML;
     var inputs = Array.from(document.getElementsByTagName('input'));
     inputs = inputs.filter(input => /^P\d{5,6}$/m.test(input.name));
-    var points = new Map(inputs.map(input => [parseInt(input.name.substring(1)), parseFloat(input.value)]));
+    var points = new Map(inputs.map(input => {
+        var matrNr = parseInt(input.name.substring(1));
+        var obj = new Object();
+        obj.points = parseFloat(input.value);
+        obj.lastName = new RegExp(matrNr + '<\\/td><td( class="trenner")?>([a-zA-Z\x7f-\xff ]+), ([a-zA-Z\x7f-\xff ]+)<').exec(html)[2];
+        obj.firstName = new RegExp(matrNr + '<\\/td><td( class="trenner")?>([a-zA-Z\x7f-\xff ]+), ([a-zA-Z\x7f-\xff ]+)<').exec(html)[3];
+        return [matrNr, obj];
+    }));
     return points;
 }
 
 function mergeData(oldData, newData, exNr = getExerciseNr()) {
     var data = new Map();
-    newData.forEach((points, matrNr) => {
+    newData.forEach((obj, matrNr) => {
         if (oldData.has(matrNr)) {
-            student = Student.from(oldData.get(matrNr));
-            student.setPoints(exNr, points);
+            var student = Student.from(oldData.get(matrNr));
+            student.setPoints(exNr, obj.points);
+            student.firstName = obj.firstName;
+            student.lastName = obj.lastName;
             data.set(matrNr, student);
         } else {
-            student = new Student(matrNr).setPoints(exNr, points);
+            student = new Student(matrNr, obj.firstName, obj.lastName).setPoints(exNr, obj.points);
             data.set(matrNr, student);
         }
     });
@@ -67,7 +77,6 @@ function saveData(data, lecture = getLecture()) {
 }
 
 function loadData(lecture = getLecture()) {
-    console.log(lecture);
     var data = JSON.parse(window.localStorage.getItem(lecture));
     if (isIterable(data)) {
         return new Map(data);
@@ -75,3 +84,11 @@ function loadData(lecture = getLecture()) {
         return new Map();
     }
 }
+
+setTimeout(() => {
+    if (window.location.pathname.endsWith('/TutorRequest')) {
+        document.getElementsByName('action')[0].addEventListener('click', function () {
+            saveData(mergeData(loadData(), extractData()));
+        })
+    }
+}, 500);
